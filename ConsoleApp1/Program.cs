b50fd6ff1522e17d489218753fe20d9469915e78
mubashir-73 <mubashirsheriff7@gmail.com>
2025-12-27 18:55:36 +0530

    Set up base and simple folder creation

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace journalcli
+{
+    internal class Program
+        {
+        static void Main(string[] args)
+            {
+            Console.WriteLine($"Number of args:{args.Length}");
+            var content = "Start typing your journal here";
+            var root = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),".journal");
+            bool fileExist =Directory.Exists(root);
+            if (!fileExist)
+            {
+                Directory.CreateDirectory(root);
+            }
+            Console.WriteLine($"The current directory is {root}");
+            var newFolderPath = Path.Combine(root, $"{DateTime.Now:yyyy}");
+            var newFilePath = Path.Combine(newFolderPath, $"{DateTime.Now:MM-dd}.md");
+            Directory.CreateDirectory(newFolderPath);
+            System.IO.File.WriteAllText($"{newFilePath}", content);
+            Console.WriteLine($"The new file path would be {newFolderPath}");
+        }
+}
+}
