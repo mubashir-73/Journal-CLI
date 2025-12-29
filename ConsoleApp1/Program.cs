b633ad4f41a590a55b4da78cc897e445b2bcb1df
mubashir-73 <mubashirsheriff7@gmail.com>
2025-12-29 10:19:16 +0530

    Created init argument to initialise Journal-CLI Directory

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,21 +4,40 @@ namespace journalcli
     internal class Program
         {
         static void Main(string[] args)
-            {
+        {
+            Journal job = new Journal();
             Console.WriteLine($"Number of args:{args.Length}");
-            var content = "Start typing your journal here";
-            var root = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),".journal");
-            bool fileExist =Directory.Exists(root);
-            if (!fileExist)
+            if (args[0] == "init")
             {
-                Directory.CreateDirectory(root);
+                job.InitializeJournal();
             }
-            Console.WriteLine($"The current directory is {root}");
-            var newFolderPath = Path.Combine(root, $"{DateTime.Now:yyyy}");
-            var newFilePath = Path.Combine(newFolderPath, $"{DateTime.Now:MM-dd}.md");
-            Directory.CreateDirectory(newFolderPath);
-            System.IO.File.WriteAllText($"{newFilePath}", content);
-            Console.WriteLine($"The new file path would be {newFolderPath}");
+            else
+            {
+                Console.WriteLine("Invalid command. Use 'init' to initialize the journal.");
+            }
+            
         }
+        
+        public class Journal
+        {
+            public bool InitializeJournal()
+            {
+                var content = "Start typing your journal here";
+                var root = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".journal");
+                bool fileExist = Directory.Exists(root);
+                if (!fileExist)
+                {
+                    Directory.CreateDirectory(root);
+                }
+                Console.WriteLine($"The current directory is {root}");
+                var newFolderPath = Path.Combine(root, $"{DateTime.Now:yyyy}");
+                var newFilePath = Path.Combine(newFolderPath, $"{DateTime.Now:MM-dd}.md");
+                Directory.CreateDirectory(newFolderPath);
+                System.IO.File.WriteAllText($"{newFilePath}", content);
+                Console.WriteLine($"The new file path would be {newFolderPath}");
+                return true;
+            }
+        }
+
 }
 }
